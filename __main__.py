f498413afefe080d89d1f044063caeac636756df
Vlad-Yeghiazaryan <vladimir.y2000@gmail.com>
2022-01-13 01:38:45 +0400

    default users

--- __main__.py
+++ __main__.py
@@ -17,19 +17,22 @@ def main(request):
   return {'scraped_data': profiles}
 
 if __name__ == '__main__':
-  copyfile('scraper_folder/.env', '.env')
-  base_url = 'https://www.linkedin.com'
-  username = config("USERNAME")
-  secret = config("PASSWORD")
-  os.remove('.env')
   try:
-    with open('scraper_folder/users.txt', 'r') as f:
-      users = f.readlines()
+    base_url = 'https://www.linkedin.com'
+    copyfile('scraper_folder/.env', '.env')
+    username = config("USERNAME")
+    secret = config("PASSWORD")
+    os.remove('.env')
+    try:
+      with open('scraper_folder/users.txt', 'r') as f:
+        users = f.readlines()
+    except:
+      users = ['https://www.linkedin.com/in/ben-affleck-3603ab200']
+    request = {'url': base_url, 'username': username,
+              'password': secret, 'profile_urls': users}
+    data = main(request)
+    import json
+    with open('scraper_folder/profiles.txt', 'w') as f:
+      json.dump(data, f)
   except:
-    users = ['https://www.linkedin.com/in/ben-affleck-3603ab200']
-  request = {'url': base_url, 'username': username,
-             'password': secret, 'profile_urls': users}
-  data = main(request)
-  import json
-  with open('scraper_folder/profiles.txt', 'w') as f:
-    json.dump(data, f)
+    print('Please expose the USERNAME and PASSWORD variables from the .env file of the scraper_folder directory.')
